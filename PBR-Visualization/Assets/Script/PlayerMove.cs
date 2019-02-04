d3cc5da1b6bb0e465f1e588b969d0d2b9c666837
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-02-04 13:17:22 +0100

    puch
    pp

---
+++ PBR-Visualization/Assets/Script/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed;
+    Vector3 direction;
+    float horizontal;
+    float vertical;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            PLayerMovement();
+        }
+    }
+
+    void PLayerMovement()
+    {
+        direction.x = -Input.GetAxis("Horizontal");
+        direction.z = -Input.GetAxis("Vertical");
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+}
