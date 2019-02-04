79a82152f2c993a86331fce5154dfe84386438c4
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-02-04 14:20:02 +0100

    puch
    ssdsd

---
+++ PBR-Visualization/Assets/Script/MatirialMapsZoom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatirialMapsZoom : MonoBehaviour
+{
+    GameObject mainCamera;
+    Vector3 newPosCamera;
+    public Vector3 adjustCam;
+    Vector3 mainCameraBasePos;
+
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        newPosCamera = transform.position + adjustCam;
+        mainCameraBasePos = mainCamera.transform.position;
+    }
+
+    void OnMouseDown()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("click " + gameObject.name);
+            //zoom
+            mainCamera.transform.position = newPosCamera;
+            GameObject.FindWithTag("UiManager").GetComponent<UIManager>().backButton.SetActive(true);
+        }
+    }
+
+    public void ZoomOut()
+    {
+        mainCamera.transform.position = mainCameraBasePos;
+        GameObject.FindWithTag("UiManager").GetComponent<UIManager>().backButton.SetActive(false);
+    }
+}
