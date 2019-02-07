801123619e82a085f1ad73404848bf0fcad05c67
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-02-07 11:40:57 +0100

    push voor naar huis
    spul

--- PBR-Visualization/Assets/Script/MatirialMapsZoom.cs
+++ PBR-Visualization/Assets/Script/MatirialMapsZoom.cs
@@ -4,78 +4,24 @@ using UnityEngine;
 
 public class MatirialMapsZoom : MonoBehaviour
 {
-    public Vector3 adjustCam;
-    public float speed;
-    public Transform parent;
-    GameObject mainCamera;
-    GameObject uiManager;
-    Vector3 newPosCamera;
-    float camMoveSpeed;
-    float stoppingDis;
-    bool advancedBool;
-    public Vector3 cameraBasePos;
+    MatirialMapsZoomBase baseClass;
+    public string nameObj;
+    public string infoObj;
 
     void Start()
     {
-        mainCamera = GameObject.FindWithTag("MainCamera");
-        uiManager = GameObject.FindWithTag("UiManager");
-        newPosCamera = transform.position + adjustCam;
-        cameraBasePos = gameObject.transform.parent.gameObject.transform.position;
-        stoppingDis = 0.01f;
+        baseClass = gameObject.transform.parent.gameObject.GetComponent<MatirialMapsZoomBase>();
     }
+
+    // geef index mee welke jij bent en welke
     void OnMouseDown()
     {
-        if (gameObject.GetComponentInChildren<PlayerLook>().freeMove == false)
+        if (GameObject.FindWithTag("Player").GetComponentInChildren<PlayerLook>().freeMove == false)
         {
             if (Input.GetButtonDown("Fire1"))
             {
-                StartCoroutine(Spread(newPosCamera));
+                baseClass.SeroundPress(nameObj, infoObj, transform);
             }
-        }
-        else
-        {
-            Debug.Log("false");
-        }
-    }
-
-    public void AdvanceTextButton()
-    {
-        uiManager.GetComponent<UIManager>().infoPbrAdvanced.SetActive(true);
-        uiManager.GetComponent<UIManager>().advancedButton.SetActive(false);
-        uiManager.GetComponent<UIManager>().infoNormal.SetActive(false);
-        advancedBool = true;
-    }
-
-    public void ZoomOut()
-    {
-        if (advancedBool == false)
-        {
-            StartCoroutine(Spread(cameraBasePos));
-        }
-        else
-        {
-            uiManager.GetComponent<UIManager>().infoPbrAdvanced.SetActive(false);
-            uiManager.GetComponent<UIManager>().advancedButton.SetActive(true);
-            uiManager.GetComponent<UIManager>().infoNormal.SetActive(true);
-            advancedBool = false;
-        }
-
-    }
-
-    IEnumerator Spread(Vector3 v)
-    {
-        while (Vector3.Distance(mainCamera.transform.position, v) >= stoppingDis)
-        {
-            camMoveSpeed = speed * Time.deltaTime;
-            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, v, camMoveSpeed);
-            yield return null;
-        }
-        GameObject panel = uiManager.GetComponent<UIManager>().infoAllPbr;
-        panel.SetActive(!panel.activeSelf);
-        if (stoppingDis <= 0.01)
-        {
-            uiManager.GetComponent<UIManager>().advancedButton.SetActive(true);
-            uiManager.GetComponent<UIManager>().infoNormal.SetActive(true);
         }
     }
 }
---
+++ PBR-Visualization/Assets/Script/MatirialMapsZoomBase.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatirialMapsZoomBase : MonoBehaviour
+{
+    [Header("Camera")]
+    public Vector3 adjustCam;
+    Vector3 cameraBasePos;
+    public float speed;
+    GameObject mainCamera;
+    Vector3 newPosCamera;
+    float camMoveSpeed;
+
+    [Header("Other")]
+    GameObject uiManager;
+    float stoppingDis;
+    bool advancedBool;
+
+    public void SeroundPress(string name, string info, Transform t)
+    {
+        //als die is ingedrukt info moet hier
+        Debug.Log("start co");
+        newPosCamera = t.position + adjustCam;
+        StartCoroutine(Spread(t.position));
+
+    }
+
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        uiManager = GameObject.FindWithTag("UiManager");
+ 
+        cameraBasePos = gameObject.transform.position;
+
+        stoppingDis = 0.01f;
+    }
+
+    IEnumerator Spread(Vector3 v)
+    {
+        while (Vector3.Distance(mainCamera.transform.position, v) >= stoppingDis)
+        {
+            Debug.Log("na while");
+            camMoveSpeed = speed * Time.deltaTime;
+            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, v, camMoveSpeed);
+            yield return null;
+        }
+        GameObject panel = uiManager.GetComponent<UIManager>().infoAllPbr;
+        panel.SetActive(!panel.activeSelf);
+        if (stoppingDis <= 0.01)
+        {
+            uiManager.GetComponent<UIManager>().advancedButton.SetActive(true);
+            uiManager.GetComponent<UIManager>().infoNormal.SetActive(true);
+        }
+    }
+
+}
+
+
+/*
+        public void AdvanceTextButton()
+    {
+        uiManager.GetComponent<UIManager>().infoPbrAdvanced.SetActive(true);
+        uiManager.GetComponent<UIManager>().advancedButton.SetActive(false);
+        uiManager.GetComponent<UIManager>().infoNormal.SetActive(false);
+        advancedBool = true;
+    }
+
+
+        public void ZoomOut()
+    {
+        if (advancedBool == false)
+        {
+            StartCoroutine(Spread(cameraBasePos));
+        }
+        else
+        {
+            uiManager.GetComponent<UIManager>().infoPbrAdvanced.SetActive(false);
+            uiManager.GetComponent<UIManager>().advancedButton.SetActive(true);
+            uiManager.GetComponent<UIManager>().infoNormal.SetActive(true);
+            advancedBool = false;
+        }
+
+    }
+
+
+*/
