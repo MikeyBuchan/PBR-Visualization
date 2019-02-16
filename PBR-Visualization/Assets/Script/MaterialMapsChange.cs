2bde360061b9a989b2b44a5d4aed5866f79587fe
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-02-16 15:13:10 +0100

    fix the matswitch
    nog geclead worden

--- PBR-Visualization/Assets/Script/MaterialMapsChange.cs
+++ PBR-Visualization/Assets/Script/MaterialMapsChange.cs
@@ -6,32 +6,29 @@ public class MaterialMapsChange : MonoBehaviour
 {
     public Material mat1;
     public Material mat2;
-    MeshRenderer mesh;
+    Renderer mesh;
 
     private void Start()
     {
-        mesh = GetComponent<MeshRenderer>();
+        mesh = GetComponent<Renderer>();
         mesh.material = mat1;
-        Debug.Log("Start this script hehehe");
     }
 
     private void Update()
     {
         SwitchMat();
+
     }
 
     public void SwitchMat()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            Debug.Log("pressed K");
             if (gameObject.GetComponent<InteractionMaterialChanger>().mayMatChange == true)
             {
-                Debug.Log("for the if");
-                if (mesh.material == mat1)
+                if (mesh.sharedMaterial == mat1)
                 {
                     mesh.material = mat2;
-                    Debug.Log("After Switch");
                 }
                 else
                 {
