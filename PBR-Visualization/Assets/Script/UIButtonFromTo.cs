cde6e4847001d2f251560a8b0a4bdb4a300c7ae3
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2019-03-04 13:54:22 +0100

    CustomizerUIGreyBox (staat in JelmerScene)

---
+++ PBR-Visualization/Assets/Script/UIButtonFromTo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonFromTo : MonoBehaviour
+{
+    public GameObject[] from;
+    public GameObject[] to;
+
+    public void OnButtonPress()
+    {
+        foreach (GameObject obj in from)
+            obj.SetActive(false);
+        foreach (GameObject obj in to)
+            obj.SetActive(true);
+    }
+}
